a538cf563348407bc583c761ccc6a00d3cb3ccc0
Chris Williams <c.williams094@gmail.com>
2018-06-29 12:16:41 -0400

    Improved tracking of articles, DOIs and PMIDs put into same file sorted by article

--- requestNCBI.py
+++ requestNCBI.py
@@ -1,16 +1,13 @@
-import requests, sys, re
+import requests, sys
 
-
-token=sys.argv[1]	#APIKey for NCBI
 pmid='id='
 db='db=pubmed&'	#We want to query the pubmed database
 	
-with open (sys.argv[2], 'r') as file:	#concatenate all of the pmids
+with open (sys.argv[1], 'r') as file:	#concatenate all of the pmids
 	for x in file:
 		pmid=pmid+str(x.strip())+','
 
-pmid=pmid[:-1]+'&'	#remove comma from the end and add & for the next variable
-
+pmid=pmid[:-1]+'&'	#remove comma from the end of our pmid list
 
 retmode='retmode=text&'	#ask for plain text
 rettype='rettype=abstract'	#ask for the abstracts
@@ -18,29 +15,25 @@ rettype='rettype=abstract'	#ask for the abstracts
 #make the html request to ncbi
 r = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?'+db+pmid+retmode+rettype)
 
+print r.status_code
+
 #write the results from ncbi to a file.  encoding helps prevent characters from being changed
 with open('abstracts', 'w+') as file:
 	file.write(r.text.encode('utf-8'))
 
+if sys.argv[2] == '1':
 
-DOIList=[]	#create a list to store the DOIs we find in the response from ncbi
-regex=r"[0-9]*[.]{1}[\w ]*[.]{1}[\w ]*[;]{1}[\d]*[(]{1}[\d]*[)]{1}[:]{1}[\d-]*[.]{1}"	#regex pattern to match first line of a new abstract
-doiCheck=False
-articleInfo=''
+	DOI='N/A'
+	count=1
 
-with open ('abstracts', 'r') as file:	#read from abstracts
-	with open ('absNoDOI', 'w+') as file2:	#create a file specifically for abstracts where we don't find DOIs
-		for line in file:
-			if 'DOI:' in line:	#DOIs are stored on separate lines, so we can check for a pattern to find them
-				DOIList.append(line)
-				doiCheck=True
-			elif re.match(regex, line) != None and 'doi' not in line.lower():	#if we find a regex match and it doesn't have a DOI associated
-				doiCheck=False	#set our doiCheck to false
-				articleInfo=line	#record the info about the line
-			if 'PMID' in line and doiCheck==False:	#if we reach the PMID without finding a DOI
-				file2.write(articleInfo.strip()+'\t'+line.split(' ')[1]+'\n')	#write the article info and the PMID to our no doi list
-				doiCheck=True
+	with open ('abstracts', 'r') as file:	#read from abstracts
+		with open ('absDOI', 'w+') as file2:	#create a file specifically for abstracts where we don't find DOIs
+			file2.write('Abstract\tDOI\tPMID'+'\n')
+			for line in file:
+				if 'DOI:' in line:	#DOIs are stored on separate lines, so we can check for a pattern to find them
+					DOI=line.split(' ')[1].strip()
+				elif 'PMID' in line:	#if we reach the PMID without finding a DOI
+					file2.write(str(count)+'\t'+DOI+'\t'+line.split(' ')[1]+'\n')	#write the article info and the PMID to our no doi list
+					count=count+1
+					DOI='N/A'
 
-with open ('absDOI', 'w+') as file:	#write our doi list to a separate file
-	for x in DOIList:
-		file.write(x)	
